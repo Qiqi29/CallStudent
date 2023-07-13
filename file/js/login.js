4ba2b518847a1ac524ebc06845ff133ad45d1d49
TheQiqi <112358908+Qiqi29@users.noreply.github.com>
2023-07-13 17:32:42 +0800

    Add files via upload

--- file/js/login.js
+++ file/js/login.js
@@ -1,31 +1,9 @@
 
-
-// 检测输入框是否输入内容
-// account.addEventListener('blur', function(){
-    //     if(account.value != ""){
-        //         loginInput();
-        //     }
-        // })
-        // password.addEventListener('blur', function(){
-            //     if(password.value != ""){
-                //         loginInput();
-                //     }
-                // })
-                
-// 检测账号密码输入框的内容
-// function loginInput(){
-    //     let loginButt = document.getElementById("loginButt");
-    //     if(Account_input.value != "" && Password_input.value != ""){
-        //         loginButt.disabled = false;
-        //     }else{
-            //         loginButt.disabled = true;
-            //     }
-            // }
-
 const Account_input = document.querySelector("#classList .className");
 const Password_input = document.getElementById("password");
 const errorTxt = document.getElementById("errorTxt");
 
+// 检测密码输入框是否有内容
 $("#loginButt").click(function(){
     if(Password_input.value == ""){
         errorTxt.innerHTML = "请输入班级密码";
@@ -33,34 +11,47 @@ $("#loginButt").click(function(){
         data();
     }
 })
-
+// 调用json文件，判断密码是否正确
 function data(){
     $.get("file/data/data.json",{},function(data){
         for(let i=0; i < data.length; i++){
             let user = data[i];
             if(Account_input.innerText == user.userName && Password_input.value == user.userPassword){
+                setCookie();
                 errorTxt.innerHTML = "";
                 window.location.href="home.html";
                 break;
             }else{
-                errorTxt.innerHTML = "账号或密码错误";
+                errorTxt.innerHTML = "× 密码错误 ×";
             }
         }
     });
 }
+// 设置班级Cookie
+function setCookie(){
+    document.cookie = "className=" + Account_input.innerText + "; expires=0";
+}
+
+
+
+
+
 
 // 显示隐藏班级列表
 const butt = document.getElementById('classList')
 const menu = document.querySelector('.classNameList')
+const arrow = document.querySelector('#classList img');
 let show = 0
 butt.addEventListener('click', ()=> {
     if(show == 0){
         menu.style.opacity = "1"
         menu.style.transform = "scale(1)"
+        arrow.style.transform = "rotate(180deg)"
         show = 1
     }else{
         menu.style.opacity = "0"
         menu.style.transform = "scale(0)"
+        arrow.style.transform = "rotate(0deg)"
         show = 0
     }
 })
